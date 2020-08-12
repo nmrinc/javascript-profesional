c2d9fd2e7713c89d2003eb22672a6ed47e402e96
Gabino Barrera <nmr.inc@icloud.com>
2020-08-11 19:23:22 -0500

    Class 7 | AutoPlay plugin added

--- assets/js/MediaPlayer.js
+++ assets/js/MediaPlayer.js
@@ -1,17 +1,42 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
 
 MediaPlayer.prototype.play = function () {
     this.media.play();
+    //document.getElementById('butt_Play').innerHTML = 'Pause';
 };
 
 MediaPlayer.prototype.pause = function () {
     this.media.pause();
+    //document.getElementById('butt_Play').innerHTML = 'Play';
 };
 
 MediaPlayer.prototype.togglePlay = function () {
     this.media.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+    this.media.muted = true;
+    //document.getElementById('butt_Mute').innerHTML = 'Unmute';
+};
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
+    //document.getElementById('butt_Mute').innerHTML = 'Mute';
+};
+
+MediaPlayer.prototype.toggleSound = function () {
+    this.media.muted ? this.unmute() : this.mute();
 };
 
 export default MediaPlayer;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins:[ new AutoPlay() ] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const butt_Play = document.getElementById('butt_Play');
+butt_Play.onclick = () => player.togglePlay();
+
+const butt_Mute = document.getElementById('butt_Mute');
+butt_Mute.onclick = () => player.toggleSound();
