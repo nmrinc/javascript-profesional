1f18f970b1ce95804d2c9e60dd79de1ce2f617eb
Gabino Barrera <nmr.inc@icloud.com>
2020-08-13 21:50:50 -0500

    Class 13 - 17 | Pictures by Braco | Explanation by Miguel Angel Soler

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,12 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins:[ new AutoPlay() ] });
+const player = new MediaPlayer({ 
+    el: video, 
+    plugins:[ 
+        new AutoPlay() 
+    ] 
+});
 
 const butt_Play = document.getElementById('butt_Play');
 butt_Play.onclick = () => player.togglePlay();
