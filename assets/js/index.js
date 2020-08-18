50bf64e03c5890801ff3bc06dcc61019ffdfd7e5
Gabino Barrera <nmr.inc@icloud.com>
2020-08-18 15:06:29 -0500

    Class 23 | Service Workers

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,9 @@ butt_Play.onclick = () => player.togglePlay();
 
 const butt_Mute = document.getElementById('butt_Mute');
 butt_Mute.onclick = () => player.toggleSound();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').catch(error => {
+        console.error(error.message);
+    });
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = 'v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    
+    //get
+    if (request.method != 'GET') return;
+
+    // Search in cache
+    event.respondWith(cachedResponse(request));
+
+    //Update cache
+    event.waitUntil(updateCache(request));
+});
+
+const precache = async () => {
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/js/index.js',
+        '/assets/js/MediaPlayer.js',
+        '/assets/js/plugins/AutoPlay.js',
+        '/assets/js/plugins/AutoPause.js',
+        '/assets/css/index.css',
+        '/assets/video/BigBuckBunny.mp4',
+    ]);
+}
+
+const cachedResponse = async (request) => {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+
+    return response || fetch(request);
+} 
+
+const updateCache = async (request) => {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    cache.put(request, response);
+} 
